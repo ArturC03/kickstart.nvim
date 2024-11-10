5a2035084f0c0f110644e7bda35e20c1b4b4e85a
ArturC03 <arturvicentecruz@proton.me>
2024-11-10 14:29:30 +0000

    Mais cenas, nem eu sei

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,17 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
-return {}
+return require('lazy').setup({
+  -- Gitsigns
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end,
+  },
+
+  -- Supermaven
+  {
+    'supermaven-inc/supermaven-nvim',
+    config = function()
+      require('supermaven-nvim').setup {}
+    end,
+  },
+}, {})
